106df9f0167b5b0ca54924f7e2ad6051e1270b4c
Jimmy Alford <jimmy.alford316@gmail.com>
2024-08-12 14:25:03 -0400

    read and loop through a file

--- compiler.java
+++ compiler.java
@@ -1,5 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 class compiler {
     public static void main(String []args) {
-        System.out.println(args[0]);
+        if (args.length == 0) {
+            System.err.println("Error: no input file");
+            return;
+        }
+
+        String filename = args[0];
+        try {
+            File file = new File(filename);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String data = scanner.nextLine();
+                System.out.println(data);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: File '${filename}' does not exist");
+            e.printStackTrace();
+        }
     }
 }
