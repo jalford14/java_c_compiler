15f675073b5e7b1a6fc8a90ed9f026c7d448266a
Jimmy Alford <jimmy.alford316@gmail.com>
2024-08-12 20:41:31 -0400

    this doesn't work but it's a skeleton of finding the largest substring for creating tokens

--- compiler.java
+++ compiler.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 class compiler {
     public static void main(String []args) {
@@ -15,12 +17,80 @@ class compiler {
             Scanner scanner = new Scanner(file);
             while (scanner.hasNextLine()) {
                 String data = scanner.nextLine();
-                System.out.println(data);
+                String substring = "";
+                int min = 0;
+                boolean inputFound = false;
+
+                for(int i = 0; i < data.length(); i++) {
+                    System.out.println(data.charAt(i));
+                    if (Character.isWhitespace(data.charAt(i))) {
+                        // ignoring whitespace
+                        if (!inputFound) { min += 1; }
+                        else {
+                            substring = data.substring(min, i - 1);
+                            System.out.println(substring);
+                            identifyToken(substring);
+                            min += substring.length() + 1;
+                            inputFound = false;
+                        }
+                    } else {
+                        inputFound = true;
+                        substring = data.substring(min, i);
+                        // when tokens are side by side
+                        // i.e. int main(var1..
+                        //              ^
+                        if (identifyToken(substring) == 0) {
+                            substring = data.substring(min, i - 1);
+                            System.out.println(substring);
+                            identifyToken(substring);
+                            min += substring.length() + 1;
+                            inputFound = false;
+                        }
+                    }
+                }
             }
             scanner.close();
         } catch (FileNotFoundException e) {
             System.out.println("Error: File '${filename}' does not exist");
             e.printStackTrace();
         }
+    }
+
+    private static int identifyToken(String input) {
+        switch(input) {
+            case String s when isMatching(s, "[a-zA-Z_]\\w*\\b") ->
+                System.out.println("Identifier");
+            case String s when isMatching(s, "[0-9]+\\b") ->
+                System.out.println("Constant");
+            case String s when isMatching(s, "int\\b") ->
+                System.out.println("int");
+            case String s when isMatching(s, "return\\b") ->
+                System.out.println("void");
+            case String s when isMatching(s, "\\(") ->
+                System.out.println("return");
+            case String s when isMatching(s, "\\)") ->
+                System.out.println("open parens");
+            case String s when isMatching(s, "\\{") ->
+                System.out.println("close parens");
+            case String s when isMatching(s, "\\}") ->
+                System.out.println("open brace");
+            case String s when isMatching(s, ";") ->
+                System.out.println("closed brace");
+            default -> {
+                System.out.println("Can't identify");
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    private static boolean matchToken(String input, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+        return matcher.matches();
+    }
+
+    private static boolean isMatching(String input, String regex) {
+        return Pattern.matches(regex, input);
     }
 }
