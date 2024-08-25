c1c8d06d528565554384df21b66d80bbd221544d
Jimmy Alford <jimmy.alford316@gmail.com>
2024-08-24 20:30:53 -0400

    flesh out abstract classes for statements and expressions

--- src/parser.java
+++ src/parser.java
@@ -1,8 +1,31 @@
 abstract class Exp {
-    abstract string name();
+    Exp(value) { this.value = value; }
+    abstract String to_string();
 }
 
 class Constant extends Exp {
+    Constant(value) { super(_int); }
+    public String to_string() {
+        return String.format("Constant(%s)", _int);
+    }
+}
+
+abstract class Statement {
+    Statement(exp) { this.exp = exp; }
+    abstract String to_string();
+}
+
+class Return extends Statement {
+    Return(exp) { super(exp); }
+    public String to_string() {
+        return String.format("Return(%s)", exp);
+    }
+}
+
+class Program {
+    String value(def) {
+        return String.format("Program(%s)", def);
+    }
 }
 
 class parser {
