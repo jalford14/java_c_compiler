54ea1fb1d2beaff37e8a69fa9516b34f5ac23ec4
Jimmy Alford <jimmy.alford316@gmail.com>
2024-08-24 22:29:23 -0400

    flesh out parse statement and expect helper

--- src/parser.java
+++ src/parser.java
@@ -1,34 +1,51 @@
 abstract class Exp {
-    Exp(value) { this.value = value; }
+    Exp(int value) { this.value = value; }
     abstract String to_string();
 }
 
 class Constant extends Exp {
-    Constant(value) { super(_int); }
+    Constant(int _int) { super(_int); }
     public String to_string() {
         return String.format("Constant(%s)", _int);
     }
 }
 
 abstract class Statement {
-    Statement(exp) { this.exp = exp; }
+    Statement(String exp) { this.exp = exp; }
     abstract String to_string();
 }
 
 class Return extends Statement {
-    Return(exp) { super(exp); }
+    Return(String exp) { super(exp); }
     public String to_string() {
         return String.format("Return(%s)", exp);
     }
 }
 
 class Program {
-    String value(def) {
+    String value(String def) {
         return String.format("Program(%s)", def);
     }
 }
 
-class parser {
-    public static void parse() {
+class Parser {
+    static String parse_statement(List<String> tokens) {
+        expect("return", tokens);
+        return_val = parse_exp(tokens);
+        expect(";", tokens);
+        return Return(return_val).to_string();
+    }
+
+    static String expect(String expected, List<String> tokens) {
+        String actual = expected.remove(0);
+        if (actual != expected) {
+            throw new InvalidSyntaxException(
+                String.format(
+                    "Unexpected token. Expected: '%s'. Actual: '%s'.",
+                    expected,
+                    actual
+                )
+            );
+        }
     }
 }
